13f57f16643a524f3c192329c3256d1afb3ecf45
Guilherme Bayma <gfbayma@gmail.com>
2015-03-30 10:55:48 -0300

    Alterado tutorial e Dados Pessoais

---
+++ AirHealthTeste/AirHealth/SincronizarViewController.m
@@ -0,0 +1,36 @@
+//
+//  SincronizarViewController.m
+//  AirHealth
+//
+//  Created by Guilherme Bayma on 3/27/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
+//
+
+#import "SincronizarViewController.h"
+
+@interface SincronizarViewController ()
+
+@end
+
+@implementation SincronizarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
